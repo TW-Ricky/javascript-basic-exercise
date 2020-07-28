b234537e2c8922e77fd6d77a7e9773880855017d
gang yue <gang.yue@thoughtworks.com>
2020-07-28 12:25:02 +0800

    [yuegang] feat: finish the exercise_04

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-plusplus */
 export default function arrayToList(array) {
   // This function creates a linked list from an array.
   //
@@ -5,6 +6,24 @@ export default function arrayToList(array) {
   //
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if (array == null) {
+    throw new Error('Creating list from undefined array');
+  }
+  if (array.length === 0) {
+    throw new Error('Creating list from empty array');
+  }
+  class Node {
+    constructor(value) {
+      this.value = value;
+      this.next = null;
+    }
+  }
+  const Linklist = new Node(array[0]);
+  let p = Linklist;
+  for (let i = 1; i < array.length; i++) {
+    const s = new Node(array[i]);
+    p.next = s;
+    p = p.next;
+  }
+  return Linklist;
 }
