8cfbdb5f5e9e4836e4ba00dd41911bc91291f89a
gang yue <gang.yue@thoughtworks.com>
2020-07-28 11:50:27 +0800

    [yuegang] feat: finish the exercise_02

--- src/exercise_02/characterCounter.js
+++ src/exercise_02/characterCounter.js
@@ -5,6 +5,18 @@ export default function countCharacter(string, prediction) {
   //
   // * Please implement this function and pass all the tests in character_counter_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if (string === undefined || string === null) return 0;
+  if (prediction !== undefined) {
+    // eslint-disable-next-line no-unused-vars
+    let cnt = 0;
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < string.length; i++) {
+      if (prediction(string[i])) {
+        // eslint-disable-next-line no-plusplus
+        cnt++;
+      }
+    }
+    return cnt;
+  }
+  return string.length;
 }
