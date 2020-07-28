d4bd8c2a70941f9592a99d58d72a21f30ce52ff8
gang yue <gang.yue@thoughtworks.com>
2020-07-28 12:43:35 +0800

    [yuegang] feat: finish the exercise_05

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-plusplus */
 export default function flattenArray(array) {
   // This function flattens a nested array into a sequence.
   //
@@ -5,6 +6,30 @@ export default function flattenArray(array) {
   //
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if (array == null) {
+    throw new Error('Flatten undefined or null array');
+  }
+  const numbers = [];
+  // for (let i = 0; i < array.length; i++) {
+  //   if (array[i].constructor === Array) {
+  //     const inarray = flattenArray(array[i]);
+  //     for (let j = 0; j < inarray.length; j++) {
+  //       numbers.push(inarray[j]);
+  //     }
+  //   } else {
+  //     numbers.push(array[i]);
+  //   }
+  // }
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].constructor === Array) {
+      const inarray = array[i];
+      for (let j = 0; j < inarray.length; j++) {
+        numbers.push(inarray[j]);
+      }
+    } else {
+      numbers.push(array[i]);
+    }
+  }
+  // console.log(numbers);
+  return numbers;
 }
