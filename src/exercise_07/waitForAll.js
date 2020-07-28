d710ba5e3ec505951617112455c0c35ac67c0e0d
gang yue <gang.yue@thoughtworks.com>
2020-07-28 22:54:46 +0800

    [yuegang] feat: finish the exercise_07

--- src/exercise_07/waitForAll.js
+++ src/exercise_07/waitForAll.js
@@ -1,3 +1,12 @@
+/* eslint-disable guard-for-in */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable consistent-return */
+/* eslint-disable max-len */
+/* eslint-disable prefer-const */
+/* eslint-disable no-loop-func */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-plusplus */
 export default function waitForAll(...promises) {
   // This function returns a promise which will be triggered when all the `promises` are completed.
   //
@@ -8,6 +17,24 @@ export default function waitForAll(...promises) {
   //
   // * Please implement this function and pass all the tests in wait_for_all_spec.js.
   // * Please do NOT modify the signature of the function.
+  for (let i = 0; i < promises.length; i++) {
+    if (!isPromise(promises[i])) {
+      throw new Error('Not all elements are promises.');
+    }
+  }
+  return Promise.allSettled(promises).then((results) => {
+    for (let i = 0; i < results.length; i++) {
+      const result = results[i];
+      if (result.status === 'rejected') {
+        return Promise.reject();
+      }
+    }
+    return Promise.resolve();
+  });
+}
 
-  throw new Error('Please delete this line and implement the function');
+
+function isPromise(obj) {
+  return !!obj && (typeof obj === 'object' || typeof obj === 'function')
+    && typeof obj.then === 'function';
 }
